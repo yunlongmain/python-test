80203e115620b7cf6c50918706145e2982f1d4ed
yunlong <yunlong@baidu.com>
2015-04-01 17:55:17 +0800

    cls

--- cls.py
+++ cls.py
@@ -13,3 +13,23 @@ summer = chicken(2)
 print(bird.__dict__)
 print(chicken.__dict__)
 print(summer.__dict__)
+
+
+class num(object):
+    def __init__(self, value):
+        self.value = value
+    def getNeg(self):
+        return -self.value
+    def setNeg(self, value):
+        self.value = -value
+    def delNeg(self):
+        print("value also deleted")
+        del self.value
+    neg = property(getNeg, setNeg, delNeg, "I'm negative")
+
+x = num(1.1)
+print(x.neg)
+x.neg = -22
+print(x.value)
+print(num.neg.__doc__)
+del x.neg
